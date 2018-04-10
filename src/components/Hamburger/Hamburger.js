2ef98e76f1184452012b7b885725614f9fc74ca9
Joe Anderson <josephiznot@gmail.com>
2018-04-10 13:24:59 -0500

    update

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -5,31 +5,44 @@ import "./Hamburger.css";
 class Hamburger extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      dropDownFlag: false
+    };
   }
   handleDropDown() {
-    var drop_down_menu = document.getElementsByClassName("drop-down-menu")[0];
-    if (drop_down_menu.style.height === "0px") {
-      drop_down_menu.style.height = "260px";
-    } else {
-      drop_down_menu.height = "0px";
-    }
+    var { dropDownFlag } = this.state;
+
+    this.setState({
+      dropDownFlag: !dropDownFlag
+    });
   }
   render() {
-    var GetIt = document.getElementsByClassName("drop-down-menu");
-    console.log(GetIt);
+    var { dropDownFlag } = this.state;
     return (
-      <button
-        className="hamburger-container"
-        onClick={() => this.handleDropDown()}
-      >
-        <h6 className="menu">MENU</h6>
-        <div className="triple-big-boy">
-          <div className="layer" />
-          <div className="layer" />
-          <div className="layer" />
-        </div>
-      </button>
+      <div>
+        <button
+          className="hamburger-container"
+          onClick={() => this.handleDropDown()}
+        >
+          <h6 className="menu">MENU</h6>
+          <div className="triple-big-boy">
+            <div className="layer" />
+            <div className="layer" />
+            <div className="layer" />
+          </div>
+        </button>
+        {dropDownFlag ? (
+          <div className="drop-down-menu" name="drop-down">
+            <ul>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+        ) : null}
+      </div>
     );
   }
 }
--- src/components/WelcomeText/WelcomeText.js
+++ src/components/WelcomeText/WelcomeText.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./WelcomeText.css";
 
-export default function WelcomeText() {
-  return (
-    <div>
-      <div className="drop-down-menu" />
-      <div className="welcome-container">
-        <h3>Welcome To Our Studio!</h3>
-        <h1>IT'S NICE TO MEET YOU</h1>
-        <button className="tell-me-more">TELL ME MORE</button>
+class WelcomeText extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dropDownFlag: false
+    };
+  }
+
+  render() {
+    var { dropDownFlag } = this.state;
+    return (
+      <div>
+        <div className="welcome-container">
+          <h3>Welcome To Our Studio!</h3>
+          <h1>IT'S NICE TO MEET YOU</h1>
+          <button className="tell-me-more">TELL ME MORE</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+export default WelcomeText;
